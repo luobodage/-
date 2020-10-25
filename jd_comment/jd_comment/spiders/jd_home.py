b1bcb41bf9efc7ead7e97d203eac113fa397ed36
luoboovo <fuyu16032001@gmail.com>
2020-10-25 12:06:40 +0800

    geigai

--- jd_comment/jd_comment/spiders/jd_home.py
+++ jd_comment/jd_comment/spiders/jd_home.py
@@ -8,18 +8,19 @@ number = 3136921
 
 class JdHomeSpider(scrapy.Spider):
     name = 'jd_home'
+
     # allowed_domains = [' ']
     # start_urls = ['http:// /']
 
     def start_requests(self, page=0):
         url = 'https://club.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98&productId={number}' \
               '&score=0&sortType=5&page={page}&pageSize=10&isShadowSku=0&fold=1'.format(
-            page = page,
-            number = number
+            page=page,
+            number=number
         )
         try:
             yield scrapy.Request(
-                url = url,
+                url=url,
                 callback=self.parse1,
                 headers={
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36'
@@ -30,8 +31,7 @@ class JdHomeSpider(scrapy.Spider):
         except:
             print('爬取失败')
 
-    def parse1(self,response):
-
+    def parse1(self, response):
 
         r = response.text
         print(r)
@@ -43,7 +43,6 @@ class JdHomeSpider(scrapy.Spider):
         print("京东评论数据：")
         for r_json_comments in r_json_comments:
             print(r_json_comments['content'])
-
 
     def parse(self, response):
         pass
